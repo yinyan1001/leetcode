33853470231e8f2462b377ce8ce36ce6bf226853
Tiwei Bie <btw@mail.ustc.edu.cn>
2014-12-02 15:42:19 +0800

    Solve the following problem:
    - 003-maximum-depth-of-binary-tree

---
+++ 003-maximum-depth-of-binary-tree/solution1.h
@@ -0,0 +1,40 @@
+
+using namespace std;
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+
+public:
+
+	int maxDepth(TreeNode *root) {
+
+		maxdepth = 0;
+		dfs(root, 0);
+
+		return (maxdepth);
+	}
+
+private:
+
+	int maxdepth;
+
+	void dfs(TreeNode *node, int depth) {
+		if (node == NULL) {
+			if (maxdepth < depth)
+				maxdepth = depth;
+			return;
+		}
+		dfs(node->left, depth+1);
+		dfs(node->right, depth+1);
+	}
+};
+
